0b44ec1b7cfdc6dd0e18bb900f9cef4daabddcf2
Kateryna Kilyk <katekilik@gmail.com>
2022-11-14 15:35:57 +0200

    Added null=True to deadline field

--- todo/models.py
+++ todo/models.py
@@ -11,7 +11,7 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.CharField(max_length=255)
     datetime = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateField()
+    deadline = models.DateField(null=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
 
